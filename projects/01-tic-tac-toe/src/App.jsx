1c6f493a906df1729dbaac5970a2391cc2e14db0
joshua-arnao <joshua.arnao@icloud.com>
2023-02-17 22:35:36 -0500

    Add standar linter

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,91 +1,91 @@
-import confetti from 'canvas-confetti';
-import { useState } from 'react';
-import './App.css';
-import { checkWinnerFrom, checkEndGame } from './logic/board';
-import { Square } from './components/Square';
-import { WinnerModal } from './components/WinnerModal';
-import { TURNS } from './constans.js';
-import { saveGameToStorage } from './logic/storage';
+import confetti from 'canvas-confetti'
+import { useState } from 'react'
+import './App.css'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { Square } from './components/Square'
+import { WinnerModal } from './components/WinnerModal'
+import { TURNS } from './constans.js'
+import { saveGameToStorage } from './logic/storage'
 
 function App() {
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board');
-    if (boardFromStorage) return JSON.parse(boardFromStorage);
-    return Array(9).fill(null);
-  });
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
 
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn');
-    return turnFromStorage ?? TURNS.X;
-  });
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
   // null es que no hay ganador, false es que hay un empate
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.X);
-    setWinner(null);
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
 
-    resetGameStorage();
-  };
+    // resetGameStorage()
+  }
 
   const updateBoard = (index) => {
     // No actualizamos la posición si ya tiene algo
-    if (board[index] || winner) return;
+    if (board[index] || winner) return
 
     // Actualizar tablero
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
     // Cambiar turno
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
 
     // Guardar partida
     saveGameToStorage({
       board: newBoard,
-      turn: newTurn,
-    });
+      turn: newTurn
+    })
 
-    //Revisar si hay ganador
-    const newWinner = checkWinnerFrom(newBoard);
+    // Revisar si hay ganador
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       // setWinner(newWinner);
-      confetti();
-      setWinner(newWinner);
+      confetti()
+      setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
-      setWinner(false);
+      setWinner(false)
     }
-  };
+  }
 
   return (
-    <main className="board">
+    <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className="game">
+      <section className='game'>
         {board.map((square, index) => {
           return (
-            <div className="cell" key={index}>
-              <span className="cell__content">
+            <div className='cell' key={index}>
+              <span className='cell__content'>
                 <Square key={index} index={index} updateBoard={updateBoard}>
                   {square}
                 </Square>
               </span>
             </div>
-          );
+          )
         })}
       </section>
 
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
       <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
