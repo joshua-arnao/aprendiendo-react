446c11270c1762fbf6212b35757b027a874725ad
joshua-arnao <joshua.arnao@icloud.com>
2023-02-17 21:12:16 -0500

    Extrat files to save

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -10,20 +10,24 @@ import { saveGameToStorage } from './logic/storage';
 function App() {
   const [board, setBoard] = useState(() => {
     const boardFromStorage = window.localStorage.getItem('board');
-    return boardFromStorage
-      ? JSON.parse(boardFromStorage)
-      : Array(9).fill(null);
+    if (boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
   });
+
   const [turn, setTurn] = useState(() => {
     const turnFromStorage = window.localStorage.getItem('turn');
     return turnFromStorage ?? TURNS.X;
   });
+
+  // null es que no hay ganador, false es que hay un empate
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    resetGameStorage();
   };
 
   const updateBoard = (index) => {
